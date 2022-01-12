68edf7159651f56c9f11ab0369052f93b9605791
Marcelo Mamedes <marcelol640@outlook.com>
2022-01-12 12:02:24 -0400

    Campos SignIn Concluido

--- AppGerenciador/src/screens/Preload/styles.js
+++ AppGerenciador/src/screens/Preload/styles.js
@@ -1,4 +1,16 @@
 import React from 'react';
-import styled from 'styled-components/native';
+import styled from 'styled-components';
 
-export const Container = styled.SafeAreaView``;
+
+
+//css é feito todo aqui
+export const Container = styled.SafeAreaView`
+        background-color: #63C2D1;
+        flex: 1;
+        justify-content: center;
+        align-items: center;
+`;
+
+export const LoadingIcon = styled.ActivityIndicator`
+margin-top: 50px;
+`;
--- AppGerenciador/src/screens/SignIn/index.js
+++ AppGerenciador/src/screens/SignIn/index.js
@@ -1,11 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text } from "react-native";
-import { Container } from "./styles";
+import BarberLogo from '../../assets/barber.svg';
+import SignInput from '../../components/SignInput';
+import EmailIcon from '../../assets/email.svg';
+import LockIcon from '../../assets/lock.svg';
+
+
+import {
+    Container,
+    InputArea,
+    CustomButton,
+    CustomButtonText,
+    SignMessageButton,
+    SignMessageButtonText,
+    SignMessageButtonTextBold
+} from "./styles";
+
 
 export default () => {
+
+    const [emailField, setEmailField] = useState('teste');
+    const [passwordField, setPasswordField] = useState('');
+
+
     return (
         <Container>
-        <Text>SignIn</Text>
+            <BarberLogo width="100%" height="160" />
+            <InputArea>
+
+                <SignInput 
+                IconSvg={EmailIcon}
+                placeholder="Digite seu e-mail"
+                value={emailField}
+                onChangeText={t=>setEmailField(t)}
+                />
+
+                <SignInput
+                IconSvg={LockIcon}
+                placeholder="Digite sua senha"
+                value={passwordField}
+                onChangeText={t=>setPasswordField(t)}
+                password={true}
+
+                />
+
+
+                <CustomButton>
+                    <CustomButtonText>Login</CustomButtonText>
+                </CustomButton>
+            </InputArea>
+
+            <SignMessageButton>
+                <SignMessageButtonText>Ainda não possui uma conta?</SignMessageButtonText>
+                <SignMessageButtonTextBold>Cadastre-se</SignMessageButtonTextBold>
+                
+            </SignMessageButton>
         </Container>
     )
 }
