43e209f73a8f49625cda8baf684db450af8907ad
Marcelo Mamedes <marcelol640@outlook.com>
2022-01-13 14:21:57 -0400

    Tela Home/Localização funcionando

--- AppGerenciador/src/screens/Home/index.js
+++ AppGerenciador/src/screens/Home/index.js
@@ -1,11 +1,87 @@
-import React from "react";
-import { Text } from "react-native";
-import { Container } from "./styles";
+import React, { useState } from "react";
+import { Platform } from "react-native";
+import {
+    Container,
+    Scroller,
 
-export default ()=>{
-    return(
+    HeaderArea,
+    HeaderTitle,
+    SearchButton,
+
+    LocationArea,
+    LocationInput,
+    LocationFinder,
+
+    LoadingIcon,
+} from "./styles";
+import SearchIcon from '../../assets/search.svg'
+import MyLocationIcon from '../../assets/my_location.svg'
+import { useNavigation } from "@react-navigation/native";
+import { request, PERMISSIONS } from 'react-native-permissions'
+import Geolocation from "@react-native-community/geolocation";
+
+export default () => {
+
+    const navigation = useNavigation();
+
+    const [locationText, setLocationText] = useState();
+    const [coords, setCoords] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [list, setList] = useState([]);
+
+    const handleLocationFinder = async () => {
+        setCords(null);
+        let result = await request(
+            Platform.OS === 'ios' ?
+                PERMISSIONS.IOS.LOCATION_WHEN_IN_USE
+                :
+                PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION
+        );
+
+        if (result == 'granted') {
+            setLoading(true);
+            setLocationText('');
+            setList([]);
+
+            Geolocation.getCurrentPosition((info) => {
+                setCoords(info.coords);
+                getBarbers();
+            });
+        }
+    }
+
+    const getBarbers = async () => {
+        
+    }
+
+    return (
         <Container>
-            <Text> Home </Text>
+            <Scroller>
+
+                <HeaderArea>
+                    <HeaderTitle numberOfLines={2}>Encontre o seu barbeiro favorito</HeaderTitle>
+                    <SearchButton onPress={() => navigation.navigate('Search')}>
+                        <SearchIcon width="26" height="26" fill="#FFF" />
+                    </SearchButton>
+                </HeaderArea>
+
+                <LocationArea>
+                    <LocationInput
+                        placeholder="Onde você está?"
+                        placeholderTextColor="#FFF"
+                        value={locationText}
+                        onChangeText={t => setLocationText(t)}
+                    />
+                    <LocationFinder onPress={handleLocationFinder}>
+                        <MyLocationIcon width="24" height="24" fill="#FFF" />
+                    </LocationFinder>
+                </LocationArea>
+                {loading &&
+
+                    <LoadingIcon size="large" color="#FFF" />
+                }
+
+            </Scroller>
         </Container>
     )
 
